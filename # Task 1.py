d7e1e9dbda0a5915dd00a998398a3453815a9924
Hicksteilchen <hicksteilchen@proton.me>
2025-02-16 14:24:33 +0100

    updated task description:   # Task 1.py

--- # Task 1.py
+++ # Task 1.py
@@ -1,1 +1,25 @@
 # Task 1
+
+# %% 
+# 1.1 a) Make a plan
+# How would you organize your hierarchy?
+# What attributes should classes have, what methods would you provide?
+# How are you going to initialize your class when instantiated?
+# Which methods could you re-use multiple times by inheritance?
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+# 1.1 b) Implement yout code
+
+# %%
+# 1.1 c) Test your code
+
+
+# %%
+
+# 1.2. Model eqations and parameters
+# implement leaky, quadratic and exponential integrate and fire neurons
+#  and plot them
