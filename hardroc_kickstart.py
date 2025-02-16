6641a52457776bbd97b5005d5a9ef7aa900340c4
Hicksteilchen <hicksteilchen@proton.me>
2025-02-16 14:13:40 +0100

    new file:   # Hard ROC.py 	new file:   # Task 1.py 	new file:   # Task 2.py 	new file:   # Task 3.py 	new file:   # Task 4.py 	new file:   Solo Challenge.ipynb 	new file:   hardroc_kickstart.py

---
+++ hardroc_kickstart.py
@@ -0,0 +1,19 @@
+# %%
+import numpy as np
+
+# example distributions
+samples_t1 = "Schnubbel"
+samples_t2 = "Dubbel"
+
+samples_a = np.array([])
+samples_b = np.array([0,])
+samples_c = np.array([1,])
+
+samples_d = np.array([0, 1, 1, 1, 1, 1])
+samples_e = np.array([1, 1, 1, 1, 1, 2])
+
+samples_f = np.array([1.001, 1.002, 1.003, 42000])
+samples_g = np.array([1.0021, 1.0028, 1.0029, 1.0027])
+
+samples_h = np.array([1, 1, 1, 1, 1, 1, 1, 1])
+samples_i = np.array([1, 1])
