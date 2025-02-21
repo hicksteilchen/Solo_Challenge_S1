53e35c762175b9719f5e213735fdd4fe390e2277
Hicksteilchen <hicksteilchen@proton.me>
2025-02-21 22:25:18 +0100

    finished:   # Task 1.py

--- # Task 1.py
+++ # Task 1.py
@@ -1,39 +1,18 @@
 # Task 1
 
-# %% 
-# 1.1 a) Make a plan
-# How would you organize your hierarchy?
-# - fr
-# What attributes should classes have, what methods would you provide?
-# methods: update membrane potential, check for spikes, general LIF
-# How are you going to initialize your class when instantiated?
-# Which methods could you re-use multiple times by inheritance?
-# 
 
 # %%
-# 1.1 b) Implement yout code
-
-# %%
-# 1.1 c) Test your code
-
-
-# %%
-
-# 1.2. Model eqations and parameters
-# implement leaky, quadratic and exponential integrate and fire neurons
-#  and plot them
-
-
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+# define classes
 
 class IAF:
     def __init__(self):
         self.V_rest = -65e-3
         self.V_thresh = -45e-3
-        self.Input = .5e-9
+        self.Input = 0.5e-9
         self.simulation_time = 100e-3
         self.dt = 0.1e-3
         self.tau = 10e-3
@@ -79,9 +58,13 @@ class QIAF(IAF):
 
     def eulers_input(self, t):
         """Compute Euler update for QIAF"""
-        return ((-self.gleak * (self.V[t - 1] - self.V_rest) * self.alpha * (self.Vc - self.V[t - 1])
-                      + self.R * self.Input)* (self.dt / self.tau))
-
+        return (
+            -self.gleak
+            * (self.V[t - 1] - self.V_rest)
+            * self.alpha
+            * (self.Vc - self.V[t - 1])
+            + self.R * self.Input
+        ) * (self.dt / self.tau)
 
 
 class EIAF(IAF):
@@ -92,13 +75,13 @@ class EIAF(IAF):
 
     def eulers_input(self, t):
         """Compute Euler update for EIAF"""
-        return ((-self.gleak * 
-                (self.V[t - 1] - self.V_rest)
-                + self.deT
-                * np.exp((self.V[t - 1] - self.V_thresh) / self.deT)
-                + self.R * self.Input )* (self.dt / self.tau))
+        return (
+            -self.gleak * (self.V[t - 1] - self.V_rest)
+            + self.deT * np.exp((self.V[t - 1] - self.V_thresh) / self.deT)
+            + self.R * self.Input
+        ) * (self.dt / self.tau)
 
-
+# %%
 # Run and plot LIAF
 instance_liaf = LIAF()
 instance_liaf.analyse_iaf()
@@ -108,6 +91,7 @@ plt.figure(figsize=(12, 8))
 plt.subplot(2, 1, 1)
 plt.plot(instance_liaf.time, instance_liaf.V, label="Membrane Potential")
 plt.axhline(instance_liaf.V_thresh, color="r", linestyle="--", label="Threshold")
+plt.title("Leaky Integrate and Fire")
 plt.ylabel("V (V)")
 plt.legend()
 plt.grid()
@@ -124,6 +108,7 @@ plt.grid()
 plt.tight_layout()
 plt.show()
 
+# %%
 # Run and plot QIAF
 instance_qiaf = QIAF()
 instance_qiaf.analyse_iaf()
@@ -133,6 +118,7 @@ plt.figure(figsize=(12, 8))
 plt.subplot(2, 1, 1)
 plt.plot(instance_qiaf.time, instance_qiaf.V, label="Membrane Potential")
 plt.axhline(instance_qiaf.V_thresh, color="r", linestyle="--", label="Threshold")
+plt.title("Quadratic Integrate and Fire")
 plt.ylabel("V (V)")
 plt.legend()
 plt.grid()
@@ -149,6 +135,7 @@ plt.grid()
 plt.tight_layout()
 plt.show()
 
+# %%
 
 # Run and plot EIAF
 instance_eiaf = EIAF()
@@ -159,6 +146,7 @@ plt.figure(figsize=(12, 8))
 plt.subplot(2, 1, 1)
 plt.plot(instance_eiaf.time, instance_eiaf.V, label="Membrane Potential")
 plt.axhline(instance_eiaf.V_thresh, color="r", linestyle="--", label="Threshold")
+plt.title("Exponential Integrate and Fire")
 plt.ylabel("V (V)")
 plt.legend()
 plt.grid()
@@ -174,3 +162,5 @@ plt.grid()
 
 plt.tight_layout()
 plt.show()
+
+# %%
